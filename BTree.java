e85e4abde9b3bae030e36528f296fddd348a203c
CybilLesbyn <cybillesbyn@onyxnode70.boisestate.edu>
2017-11-27 19:10:27 -0700

    Added BTreeNode as an inner class to BTree, also testing how the hell this works on linux

--- BTree.java
+++ BTree.java
@@ -1,9 +1,13 @@
 
 public class BTree {
-	private int degree;
+	private static int degree;
 	private BTreeNode root;
 	private int height;
 	private BTreeNode[] nodes;
+	
+	public BTree() {
+		
+	}
 	
 	public void bTreeSearch(BTreeNode x, long key) {
 		
@@ -13,5 +17,46 @@ public class BTree {
 	}
 	public void bTreeCreate() {
 		
+	}
+	
+	private class BTreeNode {
+		private NodeObject[] nodePairs;
+		private int[] children;
+		private int parent;
+		
+		public BTreeNode(int degree, int parent) {
+			BTree.degree = degree;
+			this.parent = parent;
+			
+		}
+			
+		public int getDegree() {
+			return degree;
+		}
+		
+		public boolean isLeaf() {
+			if (children.length == 0)
+				return true;
+			else
+				return false;
+		}
+		public void add(int key) {
+			if (nodePairs.length == 0) {
+				nodePairs[0].setKey(key);
+			}
+			else {
+				int i = nodePairs.length;
+				while (key < nodePairs[i].getKey()) {
+					nodePairs[i+1] = nodePairs[i];
+					i--;
+				}
+				nodePairs[i].setKey(key);
+			}
+		}
+		
+		public int getParent() {
+			return parent;
+		}
+			
 	}
 }
