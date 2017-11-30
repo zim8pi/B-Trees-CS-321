25d38e9d41a53e6228a7b58ade53b92d730dbd70
Ally Oliphant <33637182+allyoliphant@users.noreply.github.com>
2017-11-30 10:35:15 -0700

    StringBuilder capacity bug
    Originally the StringBuilder's capacity was half what it was suppose to be. SequenceLength represents the number of ATCGs in a sequence. The StringBuilder stores two numbers for every letter in a sequence (example: '00' are stored in place of an 'A'). This means that the StringBuilder's capacity needs to be twice the sequenceLength so that it can store all the zeros and ones for all the ATCGs in a sequence. Example, if a sequence was 'ATACG', StringBuilder would store '0011000110', which is twice the length of the original sequence.
    
    I also added some TODOs that list possible bugs or improvements in the code.

--- ParsingFile.java
+++ ParsingFile.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Queue;
 
@@ -19,6 +17,13 @@ public class ParsingFile {
 	private int totalSequences = 0;  //will be the max size of the array DNA
 	private NodeObject[] DNA;  //all unique sequences from file with corresponding frequencies
 	
+	public ParsingFile()
+	{
+		//default constructor
+		//TODO does this need to do anything????
+	}
+	
+	
 	/**
 	 * Read through file and create an array of NodeObjects with the unique DNA sequences
 	 * @param fileName that contains the DNA sequences
@@ -27,12 +32,13 @@ public class ParsingFile {
 	 */
 	public NodeObject[] parseDNAFile(String fileName, int sequenceLength) {
 		try {
-			subSequence = new StringBuilder(sequenceLength);
+			subSequence = new StringBuilder(2*sequenceLength);
 			buffer = new BufferedReader(new FileReader(fileName));
 				
 			//read the file until the start of the DNA sequences
 			while ((line = buffer.readLine()) != null)
 			{
+				//TODO will the DNA sequences always start on the next line after ORIGIN????
 				if (line.contains("ORIGIN"))
 				{
 					break;
@@ -43,7 +49,7 @@ public class ParsingFile {
 			while ((line = buffer.readLine()) != null)
 			{
 				//end of DNA sequences has been reached
-				//TODO will the '//' always be on a separate line than the ATGC's????
+				//TODO will the '//' always be on a separate line after the ATGC's????
 				if (line.contains("//"))
 				{
 					break;
@@ -53,15 +59,18 @@ public class ParsingFile {
 				character = line.split("");
 				
 				int i = 0;
+				//TODO should it be 'i < character.length()' instead of 'character[i] != null'?????
 				while (character[i] != null) 
 				{
 					//if the character is relevant to what we are looking for 
-					if (character[i].equals("A") || character[i].equals("a") || character[i].equals("T") || character[i].equals("t") ||
-							character[i].equals("C") || character[i].equals("c") || character[i].equals("G") || character[i].equals("g") ||
-							character[i].equals("N") || character[i].equals("n"))
+					if (character[i].equals("A") || character[i].equals("a") || 
+					    character[i].equals("T") || character[i].equals("t") || 
+					    character[i].equals("C") || character[i].equals("c") || 
+					    character[i].equals("G") || character[i].equals("g") || 
+					    character[i].equals("N") || character[i].equals("n"))
 					{
 						//remove first base if subSequence is at max length
-						if (subSequence.length() == sequenceLength)
+						if (subSequence.length() == 2*sequenceLength)
 						{
 							subSequence.delete(0, 2);
 						}
@@ -88,7 +97,9 @@ public class ParsingFile {
 							case "N":
 							case "n":
 								//reset the subSequence
-								subSequence = new StringBuilder(sequenceLength);
+								subSequence = new StringBuilder(2*sequenceLength);
+								//TODO ATCGs between two 'N' that don't total the sequenceLength????
+								//ingore them??? add them??? what????
 								break;
 						}
 						
