4881a5e1ff69d8633418604b532fb720b32708ab
CybilLesbyn <cybillesbyn@onyxnode63.boisestate.edu>
2017-12-01 18:23:37 -0700

    still not used to how git works, hopefully this doesn't ruin anything. worked on memory access, will continue

--- BTree.java
+++ BTree.java
@@ -19,10 +19,12 @@ public class BTree {
 		
 	}
 	
-	private class BTreeNode {
+	//had to make it public for the MemoryAccess file.
+	public class BTreeNode {
 		private NodeObject[] nodePairs;
 		private int[] children;
 		private int parent;
+		private int position; //for metadata
 		
 		public BTreeNode(int degree, int parent) {
 			BTree.degree = degree;
@@ -34,28 +36,36 @@ public class BTree {
 			return degree;
 		}
 		
+		public void setDegree(int degree) {
+			BTree.degree = degree;
+		}
+		
 		public boolean isLeaf() {
 			if (children.length == 0)
 				return true;
 			else
 				return false;
 		}
-		public void add(int key) {
+		public void add(long data) {
 			if (nodePairs.length == 0) {
-				nodePairs[0].setKey(key);
+				nodePairs[0].setKey(data);
 			}
 			else {
 				int i = nodePairs.length;
-				while (key < nodePairs[i].getKey()) {
+				while (data < nodePairs[i].getKey()) {
 					nodePairs[i+1] = nodePairs[i];
 					i--;
 				}
-				nodePairs[i].setKey(key);
+				nodePairs[i].setKey(data);
 			}
 		}
 		
 		public int getParent() {
 			return parent;
+		}
+		
+		public long getKey(int child) {
+			return nodePairs[child].getKey();
 		}
 			
 	}
