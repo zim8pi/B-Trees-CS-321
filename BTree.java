0df259d7873d49299eba1c17023aaac37eb4497f
Ally Oliphant <aoliphant27@gmail.com>
2017-12-08 14:53:05 -0700

    Updating BTree

--- BTree.java
+++ BTree.java
@@ -1,71 +1,139 @@
 import java.io.File;
+import java.util.Arrays;
 
-public class BTree {
+public class BTree 
+{
 	private int degree;
 	private int sequenceLength;
 	private BTreeNode root;
+	private int bTreeByteSize;  //space BTree takes in the file - holds degree, sequenceLength and root
+	private int bTreeNodeByteSize;  //space each BTreeNode takes in the file - holds nodePairs, children, parent, leaf and numKeys
+	private int numOfNodes;  //number of nodes in the tree - used to determine position of new nodes
 	private MemoryAccess rm;
 	private File file;
 	
-
 	
-	public BTree(File file) {
+	public BTree(File file, int d, int s) 
+	{
 		this.file = file;
 		rm = new MemoryAccess(file, degree);
-		
+		degree = d;
+		sequenceLength = s;
+		bTreeByteSize = 32 * d + 5;
+		bTreeNodeByteSize = 32 * d - 3;	
+		numOfNodes = 0;
 	}
 	
-	public NodeObject bTreeSearch(BTreeNode x, long key) {
-		int i = 1;
-		while (i <= x.getNumKeys() && key > x.getKey(i)) {
+	/**
+	 * @return root of B-Tree
+	 */
+	public BTreeNode getRoot()
+	{
+		return root;
+	}
+	
+	/**
+	 * Search the BTree for a key
+	 * @param x - position of the BTreeNode (0 for root)
+	 * @param key - the key that is being searched for
+	 * @return an int array - index 0 is the position of the BTreeNode and index 1 is the index of the key in the node
+	 * null is returned if not found
+	 */
+	public int[] bTreeSearch(int x, long key) 
+	{
+		int[] BTreeNodeWithKeyIndex = new int[2];
+		
+		//read the BTreeNode at position x
+		BTreeNode node = rm.readNode(x);
+		
+		int i = 0;
+		//search BTreeNode for key
+		while (i < node.getNumKeys() && key > node.getKey(i)) 
+		{
 			i = i+1;
 		}
 		
-		if(i < x.getNumKeys() && key == x.getKey(i)) {
-			return x.getObject(i);
-		} else if(x.isLeaf()) {
+		//check if key was found
+		if(i < node.getNumKeys() && key == node.getKey(i)) 
+		{
+			//key was found
+			//return BTreeNode that has the key plus the index of the key in nodePairs
+			BTreeNodeWithKeyIndex[0] = node.getPosition();  //the node with the key
+			BTreeNodeWithKeyIndex[1] = i;  //the index of the key in the node
+			return BTreeNodeWithKeyIndex;
+		} 
+		else if(node.isLeaf()) 
+		{
+			//key is not in the tree
 			return null;
-		}else {
-			//not sure how to implement this... but, the disk needs to read the child of node x, and then re-search for it using 
-			// x.child(i) (where i is the index of the child), and use the key of the child to recursively search through the tree
-			rm.readNode(x.getChild(i));
-			return bTreeSearch(x.getChild(i), x.getChild(i).getKey(i));
 		}
-		
+		else 
+		{
+			//key was not found but not at leaf yet
+			//the disk needs to read the child of node x, and then re-search for it using x.child(i)
+			//(where i is the index of the child), and recursively search through the tree
+			return bTreeSearch(node.getChild(i), key);
+		}		
 	}
-	public void bTreeInsertNonFull(BTreeNode x, long k) {
-		int i = x.getNumKeys();
-		if(x.isLeaf()) {
-			while(i >= 1 && k < x.getKey(i)) {
-				x.getObject(i+1).setKey(x.getObject(i).getKey());
-				i--;
-			}
-			x.getObject(i+1).setKey(k);
-			x.setNumKeys(x.getNumKeys() + 1);
+	
+	/**
+	 * Inserting a NodeObject into the Tree
+	 * @param x - BTreeNode key is inserted into
+	 * @param k - NodeObject being inserted
+	 */
+	public void bTreeInsertNonFull(BTreeNode x, NodeObject k) 
+	{
+		int i = x.getNumKeys() - 1;
+		
+		if(x.isLeaf()) 
+		{
+			//TODO check for max number of key-value pairs in x
+			x.addKeyPair(k.getKey(), k.getFrequency());
 			rm.writeNode(x);
-		}else {
-			while(i >= 1 && k < x.getKey(i)) {
+		}
+		else 
+		{
+			//get correct index of child
+			while(i >= 0 && k.getKey() < x.getKey(i)) 
+			{
 				i--;
 			}
 			i++;
-			rm.readNode(x.getChild(i));
-			if(x.getChild(i).getNumKeys() == (2*degree - 1)) {
+			BTreeNode child = rm.readNode(x.getChild(i));
+			if(child.getNumKeys() == (2*degree - 1)) 
+			{
 				bTreeSplitChild(x, i);
-				if(k > x.getKey(i)) {
-					i = i+1;
+				if(k.getKey() > x.getKey(i)) {
+					i = i + 1;
 				}
+				//TODO might have to reset child to the correct node after splitting
 			}
-			bTreeInsertNonFull(x.getChild(i), k);
-		}
-		
+			bTreeInsertNonFull(child, k);
+		}		
 	}
 	
-	public void bTreeInsert(BTree T, long key) {
-		BTreeNode r = new BTreeNode(degree, null);
-		BTreeNode s = new BTreeNode(degree, null);
-		r = T.root;
-		r.setPosition(0);
-		if (r.getNumKeys() == (2*degree - 1)) {
+	public void bTreeInsert(BTree T, NodeObject key) 
+	{
+		BTreeNode r = T.getRoot();
+		
+		//root is full
+		if (r.getNumKeys() == (2 * degree - 1)) 
+		{
+			//split root
+			BTreeNode left = new BTreeNode(degree, r.getPosition(), numOfNodes, true);
+			numOfNodes++;
+			BTreeNode right = new BTreeNode(degree, r.getPosition(), numOfNodes, true);
+			numOfNodes++;
+			r.setLeaf(false);
+						
+			left.setChildren(Arrays.copyOfRange(r.getChildren(), 0, (r.getNumKeys() / 2) - 1));
+			left.setAllKeyPairs(Arrays.copyOfRange(r.getAllKeyPairs(), 0, (r.getNumKeys() / 2) - 1));
+			right.setChildren(Arrays.copyOfRange(r.getChildren(), r.getNumKeys() / 2, r.getNumKeys()));
+			right.setAllKeyPairs(Arrays.copyOfRange(r.getAllKeyPairs(), r.getNumKeys() / 2, r.getNumKeys()));
+			r.setChildren(Arrays.copyOfRange(r.getChildren(), (r.getNumKeys() / 2) - 1, r.getNumKeys() / 2));
+			r.setAllKeyPairs(Arrays.copyOfRange(r.getAllKeyPairs(), r.getNumKeys() / 2, r.getNumKeys()));
+						
+			
 			s = rm.allocateNode();
 			T.root = s;
 			s.setPosition(0);
@@ -76,7 +144,9 @@ public class BTree {
 			r.setPosition(1);
 			bTreeSplitChild(s, 1);
 			bTreeInsertNonFull(s, key);
-		}else {
+		}
+		else 
+		{
 			bTreeInsertNonFull(r, key);
 		}
 	}
@@ -90,6 +160,7 @@ public class BTree {
 		T.root = x;
 		x.setPosition(0);
 	}
+	
 	//This is definitely not done. There's a lot of stuff that requires accessing the child of nodes, and I'm drawing a blank on how to do that
 	public void bTreeSplitChild(BTreeNode x, int i) {
 		BTreeNode z = new BTreeNode();
@@ -123,115 +194,218 @@ public class BTree {
 		rm.writeNode(x);
 	}
 	
-	public long convertOffset() {
-		return 40*degree + 13;
-	}
+	
+	
 	
 	//had to make it public for the MemoryAccess file.
-	public static class BTreeNode {
+	public static class BTreeNode 
+	{
 		private NodeObject[] nodePairs;  //holds the key-value pairs
-		private int[] children;
-		private int parent;
+		private int[] children;  //holds the node's children's position
+		private int parent;  //holds the node's parent's position
 		private int degree;
-		private int numKeys;
-		private int maxPairs;
-		private int maxChildren;
-		private int position; //for metadata
-		private boolean leaf;
+		private int numKeys;  //current number of key-value pairs in the node
+		private int position;  //node's position
+		private boolean leaf;  //whether the node is a leaf or not
 		
-		public BTreeNode(int degree, int parent) {
-			maxChildren = 2*degree;
-			maxPairs = 2*degree-1;
-			this.degree = degree;
-			this.parent = parent;
-		}
-			
-		public int getDegree() {
-			return degree;
-		}
-		//I added this and the setChild because I think we need to be able to acces the children to each node, but 
-		//again, I'm not too sure how to implement it
-		public BTreeNode getChild(int i) {
-			return children[i];
-		}
-		
-		public void setChild(BTreeNode x, int i) {
-			children[i] = x;
+		/**
+		 * BTreeNode constructor
+		 * @param d - degree of the tree
+		 * @param dad - position of the parent
+		 * @param spot - position of the node
+		 * @param l - if node is a leaf
+		 */
+		public BTreeNode(int d, int dad, int spot, boolean l) 
+		{
+			degree = d;
+			parent = dad;
+			nodePairs = new NodeObject[2 * d - 1];
+			children = new int[2 * d];
+			numKeys = 0;
+			position = spot;
+			leaf = l;
 		}
 		
-		public int getChildren() {
-			return children.length;
+		/**
+		 * @return true is node is a leaf, false otherwise
+		 */
+		public boolean isLeaf() 
+		{
+			return leaf;
 		}
 		
-		public void setDegree(int degree) {
-			this.degree = degree;
-		}
-		
-		public void setLeaf(boolean leafy) {
-			leaf = leafy;
-		}
-		
-		public boolean isLeaf() {
-			if (children.length == 0)
-				return true;
-			else
-				return false;
-		}
-		
-		public void add(long data) {
-			if (nodePairs.length == 0) {
-				nodePairs[0].setKey(data);
+		/**
+		 * Adds a key-value pair to the node
+		 * Doesn't check if max number of key-value pairs is reached
+		 * That should be checked for and dealt with before calling this method
+		 * @param data - the key of the key-value pair
+		 * @param frequency - the frequency of the key
+		 */
+		public void addKeyPair(long data, int frequency) 
+		{
+			if (numKeys == 0) {
+				//first key-value pair in node
+				nodePairs[0] = new NodeObject(data, frequency);
 			}
 			else {
-				int i = nodePairs.length;
-				while (data < nodePairs[i].getKey()) {
+				int i = numKeys;
+				//find spot for the new key-value pair
+				while (i > -1 && data < nodePairs[i].getKey()) {
 					nodePairs[i+1] = nodePairs[i];
 					i--;
 				}
-				nodePairs[i].setKey(data);
+				nodePairs[i] = new NodeObject(data, frequency);
 			}
 			numKeys++;
 		}
 		
-		public int getParent() {
+		/**
+		 * Get one of the node's children
+		 * @param i - int
+		 * @return the child at index i
+		 */
+		public int getChild(int i) 
+		{
+			return children[i];
+		}
+		
+		/**
+		 * Set one of the node's children
+		 * @param x - new position of child
+		 * @param i - index of child to be set
+		 */
+		public void setChild(int x, int i) 
+		{
+			children[i] = x;
+		}
+		
+		/**
+		 * Get all of the node's children
+		 * @return children - int[]
+		 */
+		public int[] getChildren() 
+		{
+			return children;
+		}
+		
+		/**
+		 * Set all of the node's children
+		 * @param c - new array of children
+		 */
+		public void setChildren(int[] c) 
+		{
+			children = c;
+		}
+		
+		/**
+		 * Set node's leaf value
+		 * @param leafy
+		 */
+		public void setLeaf(boolean leafy) 
+		{
+			leaf = leafy;
+		}
+		
+		/**
+		 * Get position of the node's parent
+		 * @return parent - int
+		 */
+		public int getParent() 
+		{
 			return parent;
 		}
 		
-		public void setParent(int dad) {
+		/**
+		 * Set the position of the node's parent
+		 * @param dad - new position of parent
+		 */
+		public void setParent(int dad) 
+		{
 			parent = dad;
 		}
 		
-		public NodeObject getObject(int i) {
+		/**
+		 * Get one key-value pair of the node at index i
+		 * @param i
+		 * @return key-value pair - NodeObject
+		 */
+		public NodeObject getKeyPair(int i) 
+		{
 			return nodePairs[i];
 		}
 		
-		public long getKey(int child) {
+		/**
+		 * Set one key-value pair of the node at index i
+		 * @param i - index being set
+		 * @param pair - new key-value pair
+		 */
+		public void setKeyPair(int i, NodeObject pair) 
+		{
+			nodePairs[i] = pair;
+		}
+		
+		/**
+		 * Get all key-value pair of the node at index i
+		 * @return key-value pairs - NodeObject[]
+		 */
+		public NodeObject[] getAllKeyPairs() 
+		{
+			return nodePairs;
+		}
+		
+		/**
+		 * Set all key-value pairs in node
+		 * @param pairs
+		 */
+		public void setAllKeyPairs(NodeObject[] pairs) 
+		{
+			nodePairs = pairs;
+		}
+		
+		/**
+		 * Get the key of one of the node's key-value pair
+		 * @param child - index of child
+		 * @return key - long
+		 */
+		public long getKey(int child) 
+		{
 			return nodePairs[child].getKey();
 		}
 		
-		public int getNumKeys() {
+		/**
+		 * Get the total number of key-value pairs in the node
+		 * @return numKeys - int
+		 */
+		public int getNumKeys() 
+		{
 			return numKeys;
 		}
 		
-		public void setNumKeys(int k) {
+		/**
+		 * Set the number of key-value pairs in the node
+		 * @param k
+		 */
+		public void setNumKeys(int k) 
+		{
 			numKeys = k;
 		}
 		
-		public int getMaxChildren() {
-			return maxChildren;
-		}
-		
-		public int getMaxPairs() {
-			return maxPairs;
-		}
-		
-		public int getPosition() {
+		/**
+		 * Get the position of the node
+		 * @return position - int
+		 */
+		public int getPosition() 
+		{
 			return position;
 		}
 		
-		public void setPosition(int pos) {
+		/**
+		 * Set the position of the node
+		 * @param pos - new position
+		 */
+		public void setPosition(int pos) 
+		{
 			position = pos;
-		}
-			
+		}			
 	}
 }
